a6cd5e7c9117933a032540416df8090010cef109
Shamar <kyudeci@gmail.com>
2019-01-09 16:52:20 -0500

    clearance isn't getting any clearer#2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  get '/database' => 'pages#database'
-  # get 'pages/home'
   root to: "pages#home"
+  get '/database' => 'pages#database'
   get '/courses'=> 'pages#courses'
   get '/main' => 'pages#main'
   post '/pages/home' => 'pages#create'
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+
+  resources :users, controller: "users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
